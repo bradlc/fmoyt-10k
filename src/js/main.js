e9ba13ef60df466ea71e691290cd156bbeefe93e
Brad Cornes <bradlc41@gmail.com>
2016-09-03 23:06:21 +0100

    Create flip function

--- src/js/main.js
+++ src/js/main.js
@@ -7,41 +7,43 @@ const movieBackPoster = document.querySelector('.js-movie-back-poster');
 
 const items = document.querySelectorAll('.js-grid-item');
 
-for (let i = 0; i < items.length; i++) {
-  items[i].addEventListener('click', e => {
-    e.preventDefault();
+function flip(e) {
+  e.preventDefault();
 
-    const poster = e.target.closest('.js-grid-item').querySelector('.js-grid-poster');
+  const poster = this.querySelector('.js-grid-poster');
 
-    // update poster
-    const image = poster.style.backgroundImage;
-    movieFront.style.backgroundImage = image;
-    movieBackPoster.style.backgroundImage = image;
+  // update poster
+  const image = poster.style.backgroundImage;
+  movieFront.style.backgroundImage = image;
+  movieBackPoster.style.backgroundImage = image;
 
-    movieContainer.classList.add('movie-container--visible');
+  movieContainer.classList.add('movie-container--visible');
 
-    const fromRect = movie.getBoundingClientRect();
-    const toRect = poster.getBoundingClientRect();
+  const fromRect = movie.getBoundingClientRect();
+  const toRect = poster.getBoundingClientRect();
 
-    const scale = 1 / (fromRect.width / toRect.width);
-    const left = toRect.left - fromRect.left;
-    const top = toRect.top - fromRect.top;
+  const scale = 1 / (fromRect.width / toRect.width);
+  const left = toRect.left - fromRect.left;
+  const top = toRect.top - fromRect.top;
 
-    movie.style.transform = `translate(${left}px, ${top}px)`;
-    movieInner.style.transform = `scale(${scale})`;
+  movie.style.transform = `translate(${left}px, ${top}px)`;
+  movieInner.style.transform = `scale(${scale})`;
 
-    // border radius
-    const borderRadius = 3 * (1 / scale);
-    movieFront.style.borderRadius = `${borderRadius}px`;
-    movieBack.style.borderRadius = `${borderRadius}px`;
+  // border radius
+  const borderRadius = 3 * (1 / scale);
+  movieFront.style.borderRadius = `${borderRadius}px`;
+  movieBack.style.borderRadius = `${borderRadius}px`;
 
-    poster.style.opacity = 0;
+  poster.style.opacity = 0;
 
-    window.requestAnimationFrame(() => {
-      movieContainer.classList.add('movie-container--animate');
-      movieInner.style.transform = 'none';
-      movie.style.transform = 'none';
-      movie.classList.add('movie--flipped');
-    });
+  window.requestAnimationFrame(() => {
+    movieContainer.classList.add('movie-container--animate');
+    movieInner.style.transform = 'none';
+    movie.style.transform = 'none';
+    movie.classList.add('movie--flipped');
   });
+}
+
+for (let i = 0; i < items.length; i++) {
+  items[i].addEventListener('click', flip);
 }
