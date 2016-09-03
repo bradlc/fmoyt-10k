0faf52a3d0a40b953366cb793c9e269f82037eee
Brad Cornes <bradlc41@gmail.com>
2016-09-03 22:39:24 +0100

    Tweak flip animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,23 @@
 const movieContainer = document.querySelector('.js-movie-container');
 const movie = document.querySelector('.js-movie');
 const movieInner = document.querySelector('.js-movie-inner');
+const movieFront = document.querySelector('.js-movie-front');
+const movieBackPoster = document.querySelector('.js-movie-back-poster');
 
 const items = document.querySelectorAll('.js-grid-item');
 
 for (let i = 0; i < items.length; i++) {
   items[i].addEventListener('click', e => {
     e.preventDefault();
-    movieContainer.classList.add('movie-container--visible');
 
     const poster = e.target.closest('.js-grid-item').querySelector('.js-grid-poster');
+
+    // update poster
+    const image = poster.style.backgroundImage;
+    movieFront.style.backgroundImage = image;
+    movieBackPoster.style.backgroundImage = image;
+
+    movieContainer.classList.add('movie-container--visible');
 
     const fromRect = movie.getBoundingClientRect();
     const toRect = poster.getBoundingClientRect();
@@ -21,9 +29,10 @@ for (let i = 0; i < items.length; i++) {
     movie.style.transform = `translate(${left}px, ${top}px)`;
     movieInner.style.transform = `scale(${scale})`;
 
+    e.target.closest('.js-grid-item').style.opacity = 0;
+
     window.requestAnimationFrame(() => {
-      movieInner.classList.add('animate');
-      movie.classList.add('animate');
+      movieContainer.classList.add('movie-container--animate');
       movieInner.style.transform = 'none';
       movie.style.transform = 'none';
       movie.classList.add('movie--flipped');
