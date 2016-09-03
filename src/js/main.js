ad6b6730a1853b1d9d8e776368f6eb3cd204cc9e
Brad Cornes <bradlc41@gmail.com>
2016-09-03 23:55:05 +0100

    Tweak flip animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+let modalActive = false;
+let activeItem = null;
+
 const movieContainer = document.querySelector('.js-movie-container');
 const movie = document.querySelector('.js-movie');
 const movieInner = document.querySelector('.js-movie-inner');
@@ -7,17 +10,34 @@ const movieBackPoster = document.querySelector('.js-movie-back-poster');
 
 const items = document.querySelectorAll('.js-grid-item');
 
+/* eslint-disable */
+function once(fn, context) {
+  let result;
+
+  return function () {
+    if (fn) {
+      result = fn.apply(context || this, arguments);
+      fn = null;
+    }
+
+    return result;
+  };
+}
+/* eslint-enable */
+
 function flip(e) {
-  e.preventDefault();
+  if (typeof e !== 'undefined') e.preventDefault();
 
   const poster = this.querySelector('.js-grid-poster');
 
   // update poster
-  const image = poster.style.backgroundImage;
-  movieFront.style.backgroundImage = image;
-  movieBackPoster.style.backgroundImage = image;
+  if (!modalActive) {
+    const image = poster.style.backgroundImage;
+    movieFront.style.backgroundImage = image;
+    movieBackPoster.style.backgroundImage = image;
 
-  movieContainer.classList.add('movie-container--visible');
+    movieContainer.classList.add('movie-container--visible');
+  }
 
   const fromRect = movie.getBoundingClientRect();
   const toRect = poster.getBoundingClientRect();
@@ -30,20 +50,38 @@ function flip(e) {
   movieInner.style.transform = `scale(${scale})`;
 
   // border radius
-  const borderRadius = 3 * (1 / scale);
-  movieFront.style.borderRadius = `${borderRadius}px`;
-  movieBack.style.borderRadius = `${borderRadius}px`;
+  if (!modalActive) {
+    const borderRadius = 3 * (1 / scale);
+    movieFront.style.borderRadius = `${borderRadius}px`;
+    movieBack.style.borderRadius = `${borderRadius}px`;
 
-  poster.style.opacity = 0;
+    poster.style.opacity = 0;
 
-  window.requestAnimationFrame(() => {
-    movieContainer.classList.add('movie-container--animate');
-    movieInner.style.transform = 'none';
-    movie.style.transform = 'none';
-    movie.classList.add('movie--flipped');
-  });
+    window.requestAnimationFrame(() => {
+      movieContainer.classList.add('movie-container--animate');
+      movieInner.style.transform = 'none';
+      movie.style.transform = 'none';
+      movie.classList.add('movie--flipped');
+    });
+  } else {
+    movie.classList.remove('movie--flipped');
+    movie.addEventListener('transitionend', once(() => {
+      poster.style.opacity = 1;
+      movieContainer.classList.remove('movie-container--animate');
+      movieContainer.classList.remove('movie-container--visible');
+      movie.style.transform = 'none';
+      movieInner.style.transform = 'none';
+    }));
+  }
+
+  modalActive = !modalActive;
+  activeItem = this;
 }
 
 for (let i = 0; i < items.length; i++) {
   items[i].addEventListener('click', flip);
 }
+
+movieContainer.addEventListener('click', () => {
+  flip.bind(activeItem)();
+});
