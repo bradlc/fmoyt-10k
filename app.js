4e8b11d736c56eaaa04e45c70f2bf922aae195b2
Brad Cornes <bradlc41@gmail.com>
2016-10-01 00:04:41 +0100

    Clean up

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const express = require('express');
 const compression = require('compression');
 const hbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-const write = require('write-file-atomic');
-const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(compression());
@@ -29,8 +27,6 @@ app.engine('handlebars', hbs({
 }));
 app.set('view engine', 'handlebars');
 app.use(express.static('webroot'));
-
-const jwtSecret = 'yolo';
 
 app.get('/', (req, res) => {
   fs.readFile('./data/all.json', 'utf8', (err, contents) => {
@@ -83,50 +79,6 @@ app.get('/:id', (req, res) => {
   });
 });
 
-app.post('/update', (req, res) => {
-  if (!req.body.token) {
-    res.send('N');
-    return;
-  }
-  jwt.verify(req.body.token, jwtSecret, (err, posts) => {
-    if (err) {
-      res.send('N');
-      return;
-    }
-
-    let i = 0;
-    let j = 0;
-    const n = posts.length;
-    const writes = [];
-
-    while (i < n) {
-      const chunk = posts.slice(i, i += 30);
-
-      writes.push(writeFile(`./data/${j}.json`, JSON.stringify(chunk)));
-
-      j++;
-    }
-
-    Promise.all(writes).then(() => {
-      res.send('written');
-    }, () => {
-      res.send('error');
-    });
-  });
-});
-
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('App listening on port 3000!');
 });
-
-
-
-
-function writeFile(filename, contents) {
-  return new Promise((resolve, reject) => {
-    write(filename, contents, err => {
-      if (err) reject(err);
-      resolve(true);
-    });
-  });
-}
